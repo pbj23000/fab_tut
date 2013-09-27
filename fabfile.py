42a93b8af9120dca9690ec362eb3fa1c002b7089
cjmf <pbj23000@gmail.com>
2013-09-27 00:18:31 -0600

    fixing import

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 from __future__ import with_statement
-from fabric.api import local, settings, abort
+from fabric.api import local, settings, abort, run, cd
 from fabric.contrib.console import confirm
 
 def test():
