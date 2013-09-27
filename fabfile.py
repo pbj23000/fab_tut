fc1cccfe33c0806147250b9553acf09746ad5628
cjmf <pbj23000@gmail.com>
2013-09-27 00:21:04 -0600

    more error handling

--- fabfile.py
+++ fabfile.py
@@ -21,6 +21,9 @@ def prepare_deploy():
 
 def deploy():
   code_dir = '/srv/django/myproject'
+  with settings(warn_only=True):
+    if run("test -d %s" % code_dir).failed:
+      run("git clone user@vcshost:/path/to/repo/.git %s" % code_dir)
   with cd(code_dir):
     run("git pull")
     run("touch app.wsgi")
