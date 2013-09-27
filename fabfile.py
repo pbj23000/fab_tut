495b1e89d15e2deb250efd867f4f9ff69ad51b34
cjmf <pbj23000@gmail.com>
2013-09-27 00:28:03 -0600

    conclusion

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,8 @@
 from __future__ import with_statement
-from fabric.api import local, settings, abort, run, cd
+from fabric.api import *
 from fabric.contrib.console import confirm
+
+env.hosts = ['my_server']
 
 def test():
   with settings(warn_only=True):
